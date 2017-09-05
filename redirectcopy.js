dae36dbe0bbec87cd47aa6f874e676868cf868a3
NguyenLy91 <maily.it0710@gmail.com>
2017-09-05 07:49:23 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://mailybrand.net'>http://mailybrand.net"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Nội dung thuộc về Mai Ly : <a href='http://mailybrand.net'>http://mailybrand.net"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
